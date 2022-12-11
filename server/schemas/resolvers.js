6be03e3b42ff972a4cbbdeffc2102e79a204a4e2
BenCottrell <bencottrell48@yahoo.com>
2022-12-11 16:56:53 -0500

    added requires to resolvers and added me query

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,17 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User } = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+              return User.findOne({ _id: context.user._id }).populate('thoughts');
+            }
+            throw new AuthenticationError('You need to be logged in!');
+          },
+        },
+        Mutation: {
+            
+        }
+    }
