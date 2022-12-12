4ef43cd1c341907630088e1485b32a3ae36dd1b7
BenCottrell <bencottrell48@yahoo.com>
2022-12-11 19:01:33 -0500

    added queries and mutations files to client side and added me query

---
+++ client/src/utils/queries.js
@@ -0,0 +1,22 @@
+import { gql } from '@apollo/client';
+
+
+// me query
+export const GET_ME = gql`
+  query me {
+    me {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
